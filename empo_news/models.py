2012ee530a985d8a1d720c7e054d871f450b1e7a
Albert Pinto i Gil <apintogil@gmail.com>
2020-03-24 20:05:13 +0100

    Add the new project files

--- empo_news/models.py
+++ empo_news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=15, primary_key=True)
+    password = models.CharField(max_length=72)
+
+
+class Contribution(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    points = models.IntegerField(default=1)
+    publication_time = models.DateTimeField('publication date')
+    url = models.CharField(max_length=500, null=True)
