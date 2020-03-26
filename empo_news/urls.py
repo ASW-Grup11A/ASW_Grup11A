036b9f3fa7795dc158ccae10e311b7e18b26ac16
marclll <sgmarcsg@gmail.com>
2020-03-26 17:24:13 +0100

    Advances in main page html

--- empo_news/urls.py
+++ empo_news/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'empo_news'
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('notimplemented', views.not_implemented, name='not_implemented')
 ]
--- empo_news/views.py
+++ empo_news/views.py
@@ -6,8 +6,12 @@ from empo_news.models import Contribution
 
 def main_page(request):
     most_points_list = Contribution.objects.order_by('-points')[:29]
-    context ={
+    context = {
         "list": most_points_list,
     }
 
     return render(request, 'empo_news/main_page.html', context);
+
+
+def not_implemented(request):
+    return HttpResponse('View not yet implemented');
