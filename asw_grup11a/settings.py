a237fa0b8f6a35b4c1b5885dcf0b8fda0bb5da45
santidrj <santi.delrey@gmail.com>
2020-03-28 20:33:29 +0100

    Update settings

--- asw_grup11a/settings.py
+++ asw_grup11a/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'emponews',
         'USER': 'admin',
-        'PASSWORD': os.environ['ASW_DB_PASWORD'],
+        'PASSWORD': os.environ['ASW_DB_PASSWORD'],
         'HOST': 'localhost',
         'PORT': '',
     }
