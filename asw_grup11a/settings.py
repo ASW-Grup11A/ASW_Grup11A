b361060e02ef9d4827ae5f9d3075dbe3f2c8ebd1
Albert Pinto i Gil <apintogil@gmail.com>
2020-05-05 19:20:57 +0200

    Try to fix CORS

--- asw_grup11a/settings.py
+++ asw_grup11a/settings.py
@@ -69,6 +69,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'asw_grup11a.urls'
 API_KEY_CUSTOM_HEADER = "HTTP_API_KEY"
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Api-Key',
+]
 
 TEMPLATES = [
     {
